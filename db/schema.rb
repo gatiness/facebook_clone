8ebc7ad43a65d9e8cc1f72c0ebc295bc8d071262
MachikoFujii <machiko.fujii@gmail.com>
2021-05-09 13:28:12 +0900

    ログインシステム実装①バリデーションとユニーク制約を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_131135) do
+ActiveRecord::Schema.define(version: 2021_05_09_041510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_05_07_131135) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
